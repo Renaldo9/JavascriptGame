eeef767c3c66a778e4dc84dbd5b27a15149d29de
Renaldo Jardim <21063944+Renaldo9@users.noreply.github.com>
2025-01-20 19:44:43 +0200

    added player movement

--- script.js
+++ script.js
@@ -38,8 +38,12 @@ window.addEventListener('load', function(){
             this.x = 20;
             this.y = 100;
             this.speedY = 0;
+            this.maxSpeed = 5;
         }
         update(){
+            if (this.game.keys.includes('ArrowUp')) this.speedY = -this.maxSpeed;
+            else if (this.game.keys.includes('ArrowDown')) this.speedY = this.maxSpeed;
+            else this.speedY = 0
             this.y += this.speedY
         }
         draw(context){
